fee773669ace8fa4e2ce082a3ae08a967489d668
martianbug <45066115+martianbug@users.noreply.github.com>
2025-09-02 11:11:27 +0200

    dataset modification

--- manipulation_dataset.py
+++ manipulation_dataset.py
@@ -2,20 +2,24 @@
 
 import pandas as pd
 from utilities import preprocess
-import ast
-DATASET = 'dataset_'
-DATASET += '28_04'
+DATASET = 'remodelling/dataset_'
+DATASET += '29_08'
 df = pd.read_csv(DATASET + '.csv')
 
-user_id_counts = df['user_id'].value_counts()
+#%%
+sent_dict = {'NEG': -1,
+             'NEU': 0,
+             'POS': 1}
 
+df['pysentimiento'] = df['pysentimiento'].map(sent_dict)
+
+#%%
+user_id_counts = df['user_id'].value_counts()
 NEW_COLUMN = 'user_id_count'
 
 df[NEW_COLUMN] = df['user_id'].map(user_id_counts)
 # df.to_csv(NAME + '_with_'+NEW_COLUMN +'.csv', index=0)
-
 df['text_preprocessed'] = df['text'].apply(preprocess)
-
 # Crear la nueva columna con la longitud de caracteres después del preprocesado
 df['text_preprocessed_length'] = df['text'].apply(preprocess).apply(len)
 df['text_original_length'] = df['text'].apply(len)
@@ -24,7 +28,7 @@ df['text_length_ratio'] = df['text_preprocessed_length'] / df['text_original_len
 # Creation of hashtags as nodes
 
 #%%
-DATE = '28_04'
+DATE = '01_09'
 df.to_csv('dataset_'+DATE+'.csv', index=0)
 
 # %%
